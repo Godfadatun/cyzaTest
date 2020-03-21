7e433e9fba8fc1edd54f708b338dd8fc1a33cfc6
Simon Moses Onazi <syflex360@gmail.com>
2020-03-21 06:02:57 +0100

    updated scafold

--- src/index.js
+++ src/index.js
@@ -1,90 +1,35 @@
 import "./scss/index.scss";
 
-window.addEventListener("DOMContentLoaded", function() {
-  // var elements = document.querySelectorAll(".responsive-nav, .modal-section");
+var x = document.querySelector(".tablink.default");
+changePage(x.id);
 
-  // Get the modal
-  var searchModal = document.querySelector("#searchModal");
-  var menuModal = document.querySelector("#menuModal");
+document.querySelector("#tabs").addEventListener("click", someFunction);
 
-  // Get the button that opens the modal
-  var searchBtn = document.querySelector("#searchBtn");
-  var menuBtn = document.querySelector("#menuBtn");
+function someFunction(event) {
+  clearTab();
+  clearButtomColor();
+  changePage(event.target.id);
+}
 
-  // Get the <span> element that closes the modal
-  var closeSearch = document.querySelector(".closeSearch");
-  var closeMenu = document.querySelector(".closeMenu");
+var i, tabcontent, tablinks;
 
-  let nav = document.querySelector("nav.main-navigation");
+function clearTab() {
+  tabcontent = document.querySelectorAll(".tabcontent");
 
-  const scrollFuc = position => {
-    if (position > 400) {
-      nav.style.backgroundColor = "#111111";
-    } else {
-      nav.style.backgroundColor = "transparent";
-    }
-  };
-
-  window.addEventListener("scroll", function() {
-    scrollFuc(pageYOffset);
-  });
-
-  function getMobileNav() {
-    let duplicateNav = nav.cloneNode(true);
-    let modalContent = menuModal.querySelector("div.modal-content");
-    let navButton = duplicateNav.querySelectorAll("button");
-    let navContainer = duplicateNav.querySelector(".responsive-nav");
-    let copyright = document.querySelector("footer .copyright").cloneNode(true);
-
-    navButton.forEach(element => {
-      navContainer.removeChild(element);
-    });
-
-    navContainer.appendChild(copyright);
-    modalContent.appendChild(duplicateNav);
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+}
 
-  // When the user clicks on the button, open the modal
-  let click = 0;
-  menuBtn.addEventListener("click", function() {
-    if (click === 0) {
-      getMobileNav();
-    }
-    menuModal.style.display = "block";
-    i++;
-  });
+function clearButtomColor(params) {
+  tablinks = document.querySelectorAll(".tablink");
 
-  searchBtn.onclick = function() {
-    searchModal.style.display = "block";
-  };
-
-  // When the user clicks on <span> (x), close the modal
-  closeSearch.onclick = function() {
-    searchModal.style.display = "none";
-  };
-
-  closeMenu.onclick = function() {
-    menuModal.style.display = "none";
-  };
-
-  // function toggleModal(modal, status){
-  //   modal.style.display = status;
-  // };
-
-  function openPage(pageName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.borderBottom = "";
   }
+}
 
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-});
+function changePage(id) {
+  document.getElementById(id + "Page").style.display = "block";
+  document.getElementById(id).style.borderBottomColor = "dodgerblue";
+}
