49b5e8cbe3789780e58fd7ce9cb3be053832e5f5
Simon Moses Onazi <syflex360@gmail.com>
2020-03-21 20:03:25 +0100

    finish swip

--- src/index.js
+++ src/index.js
@@ -32,18 +32,7 @@ function clearButtomColor(params) {
 }
 
 function changePage(id, number) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 2d9021ff370342f9d6040dedf6e77712e58ab248
-  if (number > 4) {
-    page = 5;
-  } else if (number < 2) {
-    page = 1;
-  } else {
-    page = number;
-  }
-
+  page = number;
   document.getElementById(id + "Page").style.display = "block";
   document.getElementById(id).style.borderBottomColor = "dodgerblue";
 }
@@ -55,39 +44,39 @@ function tabActivityFnc() {
   myFunction(elementId);
 }
 function myFunction(id) {
-  if(id.substr(id.length - 5) == 'close'){
+  if (id.substr(id.length - 5) == "close") {
     var res = id.replace("close", "popUp");
     document.querySelector("#" + res).style.display = "none";
   }
-  if(id.substr(id.length - 5) == 'sends'){
+  if (id.substr(id.length - 5) == "sends") {
     var data = id.replace("_sends", "");
     var res = id.replace("sends", "value");
     var res2 = id.replace("sends", "popUp");
     var text = id.replace("sends", "text");
 
-    if(document.querySelector("#"+res).value != ""){
-      localStorage.setItem(data, document.querySelector("#"+res).value);
+    if (document.querySelector("#" + res).value != "") {
+      localStorage.setItem(data, document.querySelector("#" + res).value);
     }
 
-    if(localStorage.getItem(data)){
-      document.querySelector("#"+text).innerText = localStorage.getItem(data)
+    if (localStorage.getItem(data)) {
+      document.querySelector("#" + text).innerText = localStorage.getItem(data);
     }
-    document.querySelector("#"+res).value = "";
-    document.querySelector('#'+res2).style.display = 'none';
-    }
-    else{
-      document.getElementById(id + '_popUp').style.display = 'block';
-    }
+    document.querySelector("#" + res).value = "";
+    document.querySelector("#" + res2).style.display = "none";
+  } else {
+    document.getElementById(id + "_popUp").style.display = "block";
+  }
 }
-window.onload = ()=>{
-  var form = ['phone_no', 'your_website', 'fullname', 'city_state_zip']
+window.onload = () => {
+  var form = ["phone_no", "your_website", "fullname", "city_state_zip"];
   form.map(data => {
-      if(localStorage.getItem(data)){
-        document.querySelector('#'+data+'_text').innerText = localStorage.getItem(data)
-      }
+    if (localStorage.getItem(data)) {
+      document.querySelector(
+        "#" + data + "_text"
+      ).innerText = localStorage.getItem(data);
     }
-  )
-}
+  });
+};
 
 // swip starts here
 var container = document.querySelector("#main");
