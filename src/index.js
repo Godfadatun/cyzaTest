7667a5e9522933c514b2f63d67543388d984ec09
Simon Moses Onazi <syflex360@gmail.com>
2020-03-21 19:13:43 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import "./scss/index.scss";
 
+// default
 var x = document.querySelector(".tablink.default");
-changePage(x.id);
+changePage(x.id, Number(x.dataset.number));
+// end default
 
 document.querySelector("#tabs").addEventListener("click", someFunction);
 
 function someFunction(event) {
   clearTab();
   clearButtomColor();
-  changePage(event.target.id);
+  changePage(event.target.id, Number(event.target.dataset.number));
 }
 
 var i, tabcontent, tablinks;
@@ -29,31 +31,98 @@ function clearButtomColor(params) {
   }
 }
 
-function changePage(id) {
+function changePage(id, number) {
+  
+  if (number > 4) {
+    page = 5;
+  } else if (number < 2) {
+    page = 1;
+  } else {
+    page = number;
+  }
+
   document.getElementById(id + "Page").style.display = "block";
   document.getElementById(id).style.borderBottomColor = "dodgerblue";
 }
 
-
 document.querySelector("#tab1Page").addEventListener("click", tabActivityFnc);
 
-function tabActivityFnc(){
-  var elementId = event.target.id
-  myFunction(elementId)
+function tabActivityFnc() {
+  var elementId = event.target.id;
+  myFunction(elementId);
 }
 
 function myFunction(id) {
-  if(id.substr(id.length - 5) !== 'close'){
-    document.getElementById(id + '_popUp').style.display = 'block';
+  if (id.substr(id.length - 5) !== "close") {
+    document.getElementById(id + "_popUp").style.display = "block";
   }
-  if(id.substr(id.length - 5) == 'close'){
+  if (id.substr(id.length - 5) == "close") {
     var res = id.replace("close", "popUp");
-    document.querySelector('#'+res).style.display = 'none';
+    document.querySelector("#" + res).style.display = "none";
   }
 }
 
-// function dFunction(id) {
-//   console.log(id);
+// swip starts here
+var container = document.querySelector("#main");
 
-//   document.getElementById(id + '_popUp').style.display = 'none';
-// }
+container.addEventListener("touchstart", startTouch, false);
+container.addEventListener("touchmove", moveTouch, false);
+
+// Swipe Up / Down / Left / Right
+var initialX = null;
+var initialY = null;
+
+function startTouch(e) {
+  initialX = e.touches[0].clientX;
+  initialY = e.touches[0].clientY;
+}
+
+let page = 1;
+
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+
+  if (initialY === null) {
+    return;
+  }
+
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
+
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // sliding horizontally
+    if (diffX > 0) {
+      // swiped left
+      // console.log("swiped left");
+
+      clearTab();
+      clearButtomColor();
+      changePage("tab" + (page + 1), page + 1);
+    } else {
+      // swiped right
+      // console.log("swiped right");
+      clearTab();
+      clearButtomColor();
+      changePage("tab" + (page - 1), page - 1);
+    }
+  } else {
+    // sliding vertically
+    if (diffY > 0) {
+      // swiped up
+      console.log("swiped up");
+    } else {
+      // swiped down
+      console.log("swiped down");
+    }
+  }
+
+  initialX = null;
+  initialY = null;
+
+  e.preventDefault();
+}
